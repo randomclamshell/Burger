e72b5956d634d09ca950bee42ea895f7ea55f138
KI <iqbal.mohammadk@gmail.com>
2018-11-20 11:51:04 -0500

    final push sans css

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Doublestacker", false);
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BBQ", false);
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese", false);
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,59 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var newBurger = $(this).data("newburger");
+
+    var devoured = {
+      devoured: newBurger
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burger/" + id, {
+      type: "PUT",
+      data: devoured
+    }).then(
+      function() {
+        console.log("changed sleep to", newBurger);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#ca").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burger", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burger/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted cat", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
